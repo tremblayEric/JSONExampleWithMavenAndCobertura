74769a35b431657f33bdf7e2d1bf0c255fe87640
tremblayEric <tremblay_eric@live.ca>
2013-01-18 14:28:25 -0500

    création du projet tp1Agile et premier commit sur github

---
+++ src/tp1agile/TP1Agile.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tp1agile;
+
+/**
+ *
+ * @author eric et bendjina
+ */
+public class TP1Agile {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
