bd55607a980d8ad5f55c6e6817150c903cd155c5
jpokou <j.pokou@hotmail.com>
2013-02-22 23:59:17 -0500

    -Creation class ErrorMessage regroupe tous les messages d erreurs a generer -Creation class Validation InputFileException qui traite genere des objet de type exception don le contructeur prend une chaine de caractere en parametre (message).

---
+++ src/Validation/ErrorMessage.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Validation;
+
+/**
+ *
+ * @author Boris
+ */
+public class ErrorMessage {
+    public final static String MESSAGE_ERREUR_NUMERO_CLIENT = "LE NUMERO DE CLIENT ENTRE EST NON VALIDE"; //exigence1
+    public final static String MESSAGE_ERREUR_CONTRAT = "LE CONTRAT ENTRE EST NON VALIDE"; //exigence1
+    public final static String MESSAGE_ERREUR_DATE = "LA DATE ENTREE EST NON VALIDE"; //exigence1
+    public final static String MESSAGE_ERREUR_MOIS = "LE MOIS ENTRE EST NON VALIDE"; //exigence1
+    public final static String MESSAGE_ERREUR_SIGNE_DOLLAR = "LE SIGNE DE DOLLAR EST MAL PLACE OU EST MANQUANT"; //exigence1
+    public final static String MESSAGE_ERREUR_SOIN = "LE SOIN ENTRE EST NON VALIDE"; //exigence1
+}
