d77c03a9c45267c4a7ab4a2d69a9eb4caf9c13d7
eric <tremblay_eric@live.ca>
2013-03-18 10:57:29 -0400

    Ajout d'une nouvelle classse qui fera les calcul a partir d'un objet cotenant les info necessaire.

---
+++ src/tp1agile/CalculRefundFromJavaFolder.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tp1agile;
+
+/**
+ *
+ * @author erictremblay
+ */
+public class CalculRefundFromJavaFolder {
+    
+}
