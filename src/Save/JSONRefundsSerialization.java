7742ef3918888611b63b54c113107daaba63007c
jpokou <j.pokou@hotmail.com>
2013-03-26 13:21:38 -0400

    -Ajout de la methode JSONRefundsSerializtionError qui produit le fichier output JSon d erreur

--- src/Save/JSONRefundsSerialization.java
+++ src/Save/JSONRefundsSerialization.java
@@ -57,7 +57,7 @@ public class JSONRefundsSerialization {
            JSONRefund.accumulate("soin", refund.getSoin());
            JSONRefund.accumulate("date", dateFormatMois.format(refund.getDate()));
            JSONRefund.accumulate("montant", Dollar.fromIntegerToConformStringAmount(refund.getMontant()));
-           //System.out.println(refund.getMontant());
+           System.out.println(refund.getMontant());
 
            total += refund.getMontant();
            
@@ -65,13 +65,16 @@ public class JSONRefundsSerialization {
         }
         
         folder.accumulate("remboursements", refunds);
-        
         folder.accumulate("total", Dollar.fromIntegerToConformStringAmount(total));
         
         JSONFileWriter.writeJSONObbjectToFile(folder);
         
     }
     
-    
-    
+    public static void JSONRefundsSerializationError(String errorMessage ){
+        JSONObject folder = new JSONObject();
+        folder.accumulate("Message", errorMessage);
+        JSONFileWriter.writeJSONObbjectToFile(folder);
+    }
+ 
 }
