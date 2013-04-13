bc0a6a9dcd78a7a70444aa551f91bc9436acf595
tremblayEric <tremblay_eric@live.ca>
2013-04-13 16:36:25 -0400

    TEst : redaction de test poiur check soin et checkcode

--- src/test/java/com/INF2015/app/Validation/ValidationTest.java
+++ src/test/java/com/INF2015/app/Validation/ValidationTest.java
@@ -1,44 +1,31 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.INF2015.app.Validation;
 
+import java.util.ArrayList;
 import java.util.List;
 import net.sf.json.JSONObject;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import org.junit.Ignore;
 
-/**
- *
- * @author eric et bendjina
- */
+
 public class ValidationTest {
     
     public ValidationTest() {
     }
     
-    @BeforeClass
-    public static void setUpClass() {
-    }
-    
-    @AfterClass
-    public static void tearDownClass() {
-    }
-
     /**
      * Test of checkElementsFolder method, of class Validation.
      */
-    @Test
+    @Ignore
     public void testCheckElementsFolder() throws Exception {
         System.out.println("checkElementsFolder");
         JSONObject folder = null;
         String element = "";
         Validation.checkElementsFolder(folder, element);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        
     }
 
     /**
@@ -46,13 +33,15 @@ public class ValidationTest {
      */
     @Test
     public void testCheckSoin() throws Exception {
-        System.out.println("checkSoin");
-        String soin = "";
-        String expResult = "";
-        String result = Validation.checkSoin(soin);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        
+        String soin = "0";
+        String validCare = Validation.checkSoin(soin );
+        soin = "666";
+        try{
+            Validation.checkSoin(soin );
+        }catch(Exception e){
+            assertEquals(e.getMessage() ,ErrorMessage.MESSAGE_ERROR_SOIN);
+        }
     }
 
     /**
@@ -60,32 +49,45 @@ public class ValidationTest {
      */
     @Test
     public void testCheckCode() throws Exception {
-        System.out.println("checkCode");
-        String code = "";
-        String expResult = "";
+        
+        String code = "A";
         String result = Validation.checkCode(code);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        assertEquals(code,result);
+        
+        code = "E1";
+        result = Validation.checkCode(code);
+        assertEquals(code,result);
+        
+        code = "EE";
+        try{
+            result = Validation.checkCode(code);
+        }catch(Exception e){
+            assertEquals(e.getMessage(),ErrorMessage.MESSAGE_ERROR_CODE);
+        }
+        
+        code = "qwerty";
+        try{
+            result = Validation.checkCode(code);
+        }catch(Exception e){
+            assertEquals(e.getMessage(),ErrorMessage.MESSAGE_ERROR_CODE);
+        }
+        
     }
 
     /**
      * Test of validCareList2 method, of class Validation.
      */
-    @Test
+    @Ignore
     public void testValidCareList2() {
-        System.out.println("validCareList2");
-        List expResult = null;
-        List result = Validation.validCareList2();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        
+        
+       
     }
 
     /**
      * Test of checkFolder method, of class Validation.
      */
-    @Test
+    @Ignore
     public void testCheckFolder() throws Exception {
         System.out.println("checkFolder");
         String numeroDossier = "";
@@ -99,20 +101,19 @@ public class ValidationTest {
     /**
      * Test of isInteger2 method, of class Validation.
      */
-    @Test
+    @Ignore
     public void testIsInteger2() throws Exception {
         System.out.println("isInteger2");
         String numero = "";
         String message = "";
         Validation.isInteger2(numero, message);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        
     }
 
     /**
      * Test of checkDate method, of class Validation.
      */
-    @Test
+    @Ignore
     public void testCheckDate() throws Exception {
         System.out.println("checkDate");
         String date = "";
@@ -120,28 +121,26 @@ public class ValidationTest {
         String expResult = "";
         String result = Validation.checkDate(date, month);
         assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        
     }
 
     /**
      * Test of checkMonth method, of class Validation.
      */
-    @Test
+    @Ignore
     public void testCheckMonth() throws Exception {
         System.out.println("checkMonth");
         String month = "";
         String expResult = "";
         String result = Validation.checkMonth(month);
         assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        
     }
 
     /**
      * Test of checkDateIsValid method, of class Validation.
      */
-    @Test
+    @Ignore
     public void testCheckDateIsValid() throws Exception {
         System.out.println("checkDateIsValid");
         String laDate = "";
@@ -149,14 +148,13 @@ public class ValidationTest {
         String expResult = "";
         String result = Validation.checkDateIsValid(laDate, type);
         assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        
     }
 
     /**
      * Test of checkDateMonthCoherence method, of class Validation.
      */
-    @Test
+    @Ignore
     public void testCheckDateMonthCoherence() throws Exception {
         System.out.println("checkDateMonthCoherence");
         String dateS = "";
@@ -169,14 +167,13 @@ public class ValidationTest {
     /**
      * Test of checkMontant method, of class Validation.
      */
-    @Test
+    @Ignore
     public void testCheckMontant() throws Exception {
         System.out.println("checkMontant");
         String montant = "";
         String expResult = "";
         String result = Validation.checkMontant(montant);
         assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        
     }
 }
