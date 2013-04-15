63dd96e7150e9098a873e9e26a2840553efdfa2d
tremblayEric <tremblay_eric@live.ca>
2013-04-15 11:19:36 -0400

    Factorisation: ajustement des licences

--- src/main/java/com/INF2015/app/Parsing/JSONFileReader.java
+++ src/main/java/com/INF2015/app/Parsing/JSONFileReader.java
@@ -14,7 +14,6 @@ package com.INF2015.app.Parsing;
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * 
  */
 import java.io.File;
 import java.io.FileInputStream;
